522d77fd13e379f3aa8ff336b5d8c3ccfda821c9
Rudra Swant <rudraswant18@gmail.com>
2026-02-07 12:24:30 +0530

    Remove dotenv usage for production

--- db/neon.py
+++ db/neon.py
@@ -1,17 +1,12 @@
-import psycopg2
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-print("DB_HOST =", os.getenv("DB_HOST"))
+import psycopg2
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
+        dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         port=os.getenv("DB_PORT", 5432),
         sslmode="require"
     )
-    return conn
